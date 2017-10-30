51116f935dfd22fdaf5147f58da4cd65ed6a4da0
wangtengfei <wtf123456>
2017-10-30 11:45:48 +0800

    pm2配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'server',
+      script: './server.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'root',
+      host: '60.205.207.145',
+      ref: 'origin/master',
+      repo: 'git@github.com:Taffyw/react-ssr.git',
+      path: '/root/project/react-ssr',
+      'post-deploy': 'cnpm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
