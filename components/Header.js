6ece17e21e4da8e4d674defa0703ecaa254cd33a
wangtengfei <wtf123456>
2017-10-30 17:08:55 +0800

    头部文字修改

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default () => (
   <div className='top'>
     <div className="topbar clearfix">
       <div className="wid">
-        <div className="left">您好，飞飞sd11221sdasd！</div>
+        <div className="left">您好，欢迎来到迈豪商贸！</div>
         <div className="right">
           <img src="http://13154611.s61i.faiusr.com/4/AD0Is-KiBhAEGAAgw5qpxgUo9s_1hAQwFjgS.png" alt=""/>
           <span>400-0000-000</span>
@@ -12,12 +12,13 @@ export default () => (
       </div>
     </div>
     <div className="wid">
-      <ActiveLink href='/'>首页d</ActiveLink>
+      <ActiveLink href='/'>首页</ActiveLink>
       <ActiveLink href='/about'>关于我们</ActiveLink>
       <ActiveLink href='/history'>企业文化</ActiveLink>
       <ActiveLink href='/news'>新闻动态</ActiveLink>
       <ActiveLink href='/concat'>联系我们</ActiveLink>
     </div>
+
     <style jsx>{
       `
       .top{
